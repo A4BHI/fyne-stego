b02649815df50d72dc7b4f7582041a8f810769ab
A4BHI <manthukafc@gmail.com>
2026-04-25 13:47:17 +0530

    hmm

--- ui/ui.go
+++ ui/ui.go
@@ -62,74 +62,45 @@ func (ui *appUI) LoadUI() {
 	ui.spacer.SetMinSize(fyne.NewSize(0, 20))
 }
 
+func (ui *appUI) OpenFilePicker(mainwindow fyne.Window) {
+	filepicker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+		if reader == nil {
+
+			return
+		} //if user clicks cancel
+
+		defer reader.Close()
+
+		sourceImage, err := io.ReadAll(reader)
+		if err != nil {
+			fmt.Println(err)
+
+		}
+
+		fmt.Println(sourceImage)
+		os.WriteFile("/home/a4bhi/Desktop/fyne-stego/test.png", sourceImage, 0644)
+	}, mainwindow)
+	filepicker.SetFilter(ui.filefilter)
+	filepicker.SetConfirmText("Choose Image")
+	filepicker.Show()
+
+}
+
 func (ui *appUI) BuildUI(mainwindow fyne.Window) fyne.CanvasObject {
 
 	ui.encodeTab.selectImageBtn = widget.NewButton("select image", func() {
-
-		filepicker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			if reader == nil {
-
-				return
-			} //if user clicks cancel
-
-			defer reader.Close()
-
-			sourceImage, err := io.ReadAll(reader)
-			if err != nil {
-				fmt.Println(err)
-
-			}
-
-			fmt.Println(sourceImage)
-			os.WriteFile("/home/a4bhi/Desktop/fyne-stego/test.png", sourceImage, 0644)
-		}, mainwindow)
-		filepicker.SetFilter(ui.filefilter)
-		filepicker.SetConfirmText("Choose Image")
-		filepicker.Show()
-
+		ui.OpenFilePicker(mainwindow)
 	})
 
 	ui.encodeTab.selectFilebtn = widget.NewButton("Pick File", func() {
 
-		filepicker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			if reader == nil {
-
-				return
-			} //if user clicks cancel
-
-			defer reader.Close()
-
-			sourceImage, err := io.ReadAll(reader)
-			if err != nil {
-				fmt.Println(err)
-
-			}
-
-			fmt.Println(sourceImage)
-			os.WriteFile("/home/a4bhi/Desktop/fyne-stego/test.png", sourceImage, 0644)
-		}, mainwindow)
-		filepicker.SetFilter(ui.filefilter)
-		filepicker.SetConfirmText("Choose Image")
-		filepicker.Show()
+		ui.OpenFilePicker(mainwindow)
 
 	})
 
 	ui.decodeTab.pickFileButtpm = widget.NewButton("Pick File", func() {
 
-		filepicker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-
-			sourceImage, err := io.ReadAll(reader)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			fmt.Println(sourceImage)
-			os.WriteFile("/home/a4bhi/Desktop/fyne-stego/test.png", sourceImage, 0644)
-		}, mainwindow)
-		filepicker.SetFilter(ui.filefilter)
-		filepicker.SetConfirmText("Choose Image")
-		filepicker.Show()
-
+		ui.OpenFilePicker(mainwindow)
 	})
 
 	imageLayout := container.NewGridWithColumns(2, ui.encodeTab.sourceImageLabel, ui.encodeTab.selectImageBtn)
